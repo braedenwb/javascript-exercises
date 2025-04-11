79394a5050bf3bdc2cf8156bd3f653fc5b0d5bfe
braedenwb <braedenwboyce@gmail.com>
2025-04-11 06:05:54 -0500

    Finished exercises 1 through 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max))
+    {
+        return "ERROR";
+    }
+    if (min < 0 || max < 0)
+    {
+        return "ERROR";
+    }
+    if (min > max)
+    {
+        const newMax = min;
+        min = max;
+        max = newMax;
+    }
+    
+    
+    let sum = 0;
 
+    for (let i = min; i <= max; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
